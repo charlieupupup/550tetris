d899a6205f5088bce8f03e09951068df8ce753a0
charlieupupup <41690273+charlieupupup@users.noreply.github.com>
2019-12-05 00:53:06 -0500

    Update block_pos.v

--- tetris_v/block_pos.v
+++ tetris_v/block_pos.v
@@ -14,20 +14,26 @@ input clk, drop, left, right;
 input [4:0] block_pos_x_in, block_pos_y_in;
 
 output [4:0] block_pos_x_out, block_pos_y_out;
+reg [4:0] block_pos_x_out, block_pos_y_out;
 
-always @ (posedge clk) {
+initial begin
+    block_pos_x_out = 5;
+    block_pos_y_out = 0;
+end
+
+always @ (posedge clk) 
     if (drop == 1) begin
     block_pos_y_out = block_pos_y_in + 1;
         
     end
 
-    if (left == 1) begin
+    else if (left == 1) begin
     block_pos_x_out = block_pos_x_in - 1;   
     end
 
-    if (right == 1) begin
+    else if (right == 1) begin
     block_pos_x_out = block_pos_x_in + 1;   
     end
-}
+
 
 endmodule // block_pos
