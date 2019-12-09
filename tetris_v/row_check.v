d2ddd596e232d693b99e58eec671257dc3a193a7
Zhengyang Liu <31042884+zyLiu6707@users.noreply.github.com>
2019-12-09 17:12:30 -0500

    完成了消行的核心代码

--- tetris_v/row_check.v
+++ tetris_v/row_check.v
@@ -1,3 +1,8 @@
+//模块直接对background[399:0]进行操作
+//故此处的field的名称应最后改为记录游戏背景板
+//的400的点阵的寄存器阵列的名称
+//此模块代码最终需要整合进描述游戏背景板的模块代码中去
+
 module row_check(
    input      [399:0]  matrix_in,
    output reg [399:0]  matrix_out,
@@ -14,7 +19,7 @@ generate for(gv_i = 0; gv_i < 20; gv_i = gv_i + 1)
     end
 endgenerate
 
-assign is_any_line_full = |line[19:0];
+assign is_any_line_full = |line[19:0]; //is_any_line_full is 1'd1 when at lease one line is full
 
 always@ (posedge clk or negedge rst_n) begin
     if( !rst_n ) begin
@@ -118,11 +123,11 @@ always@ (posedge clk or negedge rst_n) begin
             total_line_num <= total_line_num + 1; //full line recorder
         end
 
-        if(line[2]) begin
+        if(line[3]) begin
             field[19:0]     <= 20'd0;
             field[39:20]    <= field[19:0]   ;
             field[59:40]    <= field[39:20]  ;
-            field[79:60]    <= field[79:60]  ;
+            field[79:60]    <= field[59:40]  ;
             field[99:80]    <= field[99:80]  ;
             field[119:100]  <= field[119:100];
             field[139:120]  <= field[139:120];
@@ -142,7 +147,406 @@ always@ (posedge clk or negedge rst_n) begin
 
             total_line_num <= total_line_num + 1; //full line recorder
         end
+
+        if(line[4]) begin
+            field[19:0]     <= 20'd0;
+            field[39:20]    <= field[19:0]   ;
+            field[59:40]    <= field[39:20]  ;
+            field[79:60]    <= field[59:40]  ;
+            field[99:80]    <= field[79:60]  ;
+            field[119:100]  <= field[119:100];
+            field[139:120]  <= field[139:120];
+            field[159:140]  <= field[159:140];
+            field[179:160]  <= field[179:160];
+            field[199:180]  <= field[199:180];
+            field[219:200]  <= field[219:200];
+            field[239:220]  <= field[239:220];
+            field[259:240]  <= field[259:240];
+            field[279:260]  <= field[279:260];
+            field[299:280]  <= field[299:280];
+            field[319:300]  <= field[319:300];
+            field[339:320]  <= field[339:320];
+            field[359:340]  <= field[359:340];
+            field[379:360]  <= field[379:360];
+            field[399:380]  <= field[399:380];
+
+            total_line_num <= total_line_num + 1; //full line recorder
+        end
         
+        if(line[5]) begin
+            field[19:0]     <= 20'd0;
+            field[39:20]    <= field[19:0]   ;
+            field[59:40]    <= field[39:20]  ;
+            field[79:60]    <= field[59:40]  ;
+            field[99:80]    <= field[79:60]  ;
+            field[119:100]  <= field[99:80]  ;
+            field[139:120]  <= field[139:120];
+            field[159:140]  <= field[159:140];
+            field[179:160]  <= field[179:160];
+            field[199:180]  <= field[199:180];
+            field[219:200]  <= field[219:200];
+            field[239:220]  <= field[239:220];
+            field[259:240]  <= field[259:240];
+            field[279:260]  <= field[279:260];
+            field[299:280]  <= field[299:280];
+            field[319:300]  <= field[319:300];
+            field[339:320]  <= field[339:320];
+            field[359:340]  <= field[359:340];
+            field[379:360]  <= field[379:360];
+            field[399:380]  <= field[399:380];
+
+            total_line_num <= total_line_num + 1; //full line recorder
+        end
+
+        if(line[6]) begin
+            field[19:0]     <= 20'd0;
+            field[39:20]    <= field[19:0]   ;
+            field[59:40]    <= field[39:20]  ;
+            field[79:60]    <= field[59:40]  ;
+            field[99:80]    <= field[79:60]  ;
+            field[119:100]  <= field[99:80]  ;
+            field[139:120]  <= field[119:100];
+            field[159:140]  <= field[159:140];
+            field[179:160]  <= field[179:160];
+            field[199:180]  <= field[199:180];
+            field[219:200]  <= field[219:200];
+            field[239:220]  <= field[239:220];
+            field[259:240]  <= field[259:240];
+            field[279:260]  <= field[279:260];
+            field[299:280]  <= field[299:280];
+            field[319:300]  <= field[319:300];
+            field[339:320]  <= field[339:320];
+            field[359:340]  <= field[359:340];
+            field[379:360]  <= field[379:360];
+            field[399:380]  <= field[399:380];
+
+            total_line_num <= total_line_num + 1; //full line recorder
+        end
+
+        if(line[7]) begin
+            field[19:0]     <= 20'd0;
+            field[39:20]    <= field[19:0]   ;
+            field[59:40]    <= field[39:20]  ;
+            field[79:60]    <= field[59:40]  ;
+            field[99:80]    <= field[79:60]  ;
+            field[119:100]  <= field[99:80]  ;
+            field[139:120]  <= field[119:100];
+            field[159:140]  <= field[139:120];
+            field[179:160]  <= field[179:160];
+            field[199:180]  <= field[199:180];
+            field[219:200]  <= field[219:200];
+            field[239:220]  <= field[239:220];
+            field[259:240]  <= field[259:240];
+            field[279:260]  <= field[279:260];
+            field[299:280]  <= field[299:280];
+            field[319:300]  <= field[319:300];
+            field[339:320]  <= field[339:320];
+            field[359:340]  <= field[359:340];
+            field[379:360]  <= field[379:360];
+            field[399:380]  <= field[399:380];
+
+            total_line_num <= total_line_num + 1; //full line recorder
+        end
+
+        if(line[8]) begin
+            field[19:0]     <= 20'd0;
+            field[39:20]    <= field[19:0]   ;
+            field[59:40]    <= field[39:20]  ;
+            field[79:60]    <= field[59:40]  ;
+            field[99:80]    <= field[79:60]  ;
+            field[119:100]  <= field[99:80]  ;
+            field[139:120]  <= field[119:100];
+            field[159:140]  <= field[139:120];
+            field[179:160]  <= field[159:140];
+            field[199:180]  <= field[199:180];
+            field[219:200]  <= field[219:200];
+            field[239:220]  <= field[239:220];
+            field[259:240]  <= field[259:240];
+            field[279:260]  <= field[279:260];
+            field[299:280]  <= field[299:280];
+            field[319:300]  <= field[319:300];
+            field[339:320]  <= field[339:320];
+            field[359:340]  <= field[359:340];
+            field[379:360]  <= field[379:360];
+            field[399:380]  <= field[399:380];
+
+            total_line_num <= total_line_num + 1; //full line recorder
+        end
+
+        if(line[9]) begin
+            field[19:0]     <= 20'd0;
+            field[39:20]    <= field[19:0]   ;
+            field[59:40]    <= field[39:20]  ;
+            field[79:60]    <= field[59:40]  ;
+            field[99:80]    <= field[79:60]  ;
+            field[119:100]  <= field[99:80]  ;
+            field[139:120]  <= field[119:100];
+            field[159:140]  <= field[139:120];
+            field[179:160]  <= field[159:140];
+            field[199:180]  <= field[179:160];
+            field[219:200]  <= field[219:200];
+            field[239:220]  <= field[239:220];
+            field[259:240]  <= field[259:240];
+            field[279:260]  <= field[279:260];
+            field[299:280]  <= field[299:280];
+            field[319:300]  <= field[319:300];
+            field[339:320]  <= field[339:320];
+            field[359:340]  <= field[359:340];
+            field[379:360]  <= field[379:360];
+            field[399:380]  <= field[399:380];
+
+            total_line_num <= total_line_num + 1; //full line recorder
+        end
+
+        if(line[10]) begin
+            field[19:0]     <= 20'd0;
+            field[39:20]    <= field[19:0]   ;
+            field[59:40]    <= field[39:20]  ;
+            field[79:60]    <= field[59:40]  ;
+            field[99:80]    <= field[79:60]  ;
+            field[119:100]  <= field[99:80]  ;
+            field[139:120]  <= field[119:100];
+            field[159:140]  <= field[139:120];
+            field[179:160]  <= field[159:140];
+            field[199:180]  <= field[179:160];
+            field[219:200]  <= field[199:180];
+            field[239:220]  <= field[239:220];
+            field[259:240]  <= field[259:240];
+            field[279:260]  <= field[279:260];
+            field[299:280]  <= field[299:280];
+            field[319:300]  <= field[319:300];
+            field[339:320]  <= field[339:320];
+            field[359:340]  <= field[359:340];
+            field[379:360]  <= field[379:360];
+            field[399:380]  <= field[399:380];
+
+            total_line_num <= total_line_num + 1; //full line recorder
+        end
+
+        if(line[11]) begin
+            field[19:0]     <= 20'd0;
+            field[39:20]    <= field[19:0]   ;
+            field[59:40]    <= field[39:20]  ;
+            field[79:60]    <= field[59:40]  ;
+            field[99:80]    <= field[79:60]  ;
+            field[119:100]  <= field[99:80]  ;
+            field[139:120]  <= field[119:100];
+            field[159:140]  <= field[139:120];
+            field[179:160]  <= field[159:140];
+            field[199:180]  <= field[179:160];
+            field[219:200]  <= field[199:180];
+            field[239:220]  <= field[219:200];
+            field[259:240]  <= field[259:240];
+            field[279:260]  <= field[279:260];
+            field[299:280]  <= field[299:280];
+            field[319:300]  <= field[319:300];
+            field[339:320]  <= field[339:320];
+            field[359:340]  <= field[359:340];
+            field[379:360]  <= field[379:360];
+            field[399:380]  <= field[399:380];
+
+            total_line_num <= total_line_num + 1; //full line recorder
+        end
+
+        if(line[12]) begin
+            field[19:0]     <= 20'd0;
+            field[39:20]    <= field[19:0]   ;
+            field[59:40]    <= field[39:20]  ;
+            field[79:60]    <= field[59:40]  ;
+            field[99:80]    <= field[79:60]  ;
+            field[119:100]  <= field[99:80]  ;
+            field[139:120]  <= field[119:100];
+            field[159:140]  <= field[139:120];
+            field[179:160]  <= field[159:140];
+            field[199:180]  <= field[179:160];
+            field[219:200]  <= field[199:180];
+            field[239:220]  <= field[219:200];
+            field[259:240]  <= field[239:220];
+            field[279:260]  <= field[279:260];
+            field[299:280]  <= field[299:280];
+            field[319:300]  <= field[319:300];
+            field[339:320]  <= field[339:320];
+            field[359:340]  <= field[359:340];
+            field[379:360]  <= field[379:360];
+            field[399:380]  <= field[399:380];
+
+            total_line_num <= total_line_num + 1; //full line recorder
+        end
+
+        if(line[13]) begin
+            field[19:0]     <= 20'd0;
+            field[39:20]    <= field[19:0]   ;
+            field[59:40]    <= field[39:20]  ;
+            field[79:60]    <= field[59:40]  ;
+            field[99:80]    <= field[79:60]  ;
+            field[119:100]  <= field[99:80]  ;
+            field[139:120]  <= field[119:100];
+            field[159:140]  <= field[139:120];
+            field[179:160]  <= field[159:140];
+            field[199:180]  <= field[179:160];
+            field[219:200]  <= field[199:180];
+            field[239:220]  <= field[219:200];
+            field[259:240]  <= field[239:220];
+            field[279:260]  <= field[259:240];
+            field[299:280]  <= field[299:280];
+            field[319:300]  <= field[319:300];
+            field[339:320]  <= field[339:320];
+            field[359:340]  <= field[359:340];
+            field[379:360]  <= field[379:360];
+            field[399:380]  <= field[399:380];
+
+            total_line_num <= total_line_num + 1; //full line recorder
+        end
+
+        if(line[14]) begin
+            field[19:0]     <= 20'd0;
+            field[39:20]    <= field[19:0]   ;
+            field[59:40]    <= field[39:20]  ;
+            field[79:60]    <= field[59:40]  ;
+            field[99:80]    <= field[79:60]  ;
+            field[119:100]  <= field[99:80]  ;
+            field[139:120]  <= field[119:100];
+            field[159:140]  <= field[139:120];
+            field[179:160]  <= field[159:140];
+            field[199:180]  <= field[179:160];
+            field[219:200]  <= field[199:180];
+            field[239:220]  <= field[219:200];
+            field[259:240]  <= field[239:220];
+            field[279:260]  <= field[259:240];
+            field[299:280]  <= field[279:260];
+            field[319:300]  <= field[319:300];
+            field[339:320]  <= field[339:320];
+            field[359:340]  <= field[359:340];
+            field[379:360]  <= field[379:360];
+            field[399:380]  <= field[399:380];
+
+            total_line_num <= total_line_num + 1; //full line recorder
+        end
+        
+        if(line[15]) begin
+            field[19:0]     <= 20'd0;
+            field[39:20]    <= field[19:0]   ;
+            field[59:40]    <= field[39:20]  ;
+            field[79:60]    <= field[59:40]  ;
+            field[99:80]    <= field[79:60]  ;
+            field[119:100]  <= field[99:80]  ;
+            field[139:120]  <= field[119:100];
+            field[159:140]  <= field[139:120];
+            field[179:160]  <= field[159:140];
+            field[199:180]  <= field[179:160];
+            field[219:200]  <= field[199:180];
+            field[239:220]  <= field[219:200];
+            field[259:240]  <= field[239:220];
+            field[279:260]  <= field[259:240];
+            field[299:280]  <= field[279:260];
+            field[319:300]  <= field[299:280];
+            field[339:320]  <= field[339:320];
+            field[359:340]  <= field[359:340];
+            field[379:360]  <= field[379:360];
+            field[399:380]  <= field[399:380];
+
+            total_line_num <= total_line_num + 1; //full line recorder
+        end
+
+        if(line[16]) begin
+            field[19:0]     <= 20'd0;
+            field[39:20]    <= field[19:0]   ;
+            field[59:40]    <= field[39:20]  ;
+            field[79:60]    <= field[59:40]  ;
+            field[99:80]    <= field[79:60]  ;
+            field[119:100]  <= field[99:80]  ;
+            field[139:120]  <= field[119:100];
+            field[159:140]  <= field[139:120];
+            field[179:160]  <= field[159:140];
+            field[199:180]  <= field[179:160];
+            field[219:200]  <= field[199:180];
+            field[239:220]  <= field[219:200];
+            field[259:240]  <= field[239:220];
+            field[279:260]  <= field[259:240];
+            field[299:280]  <= field[279:260];
+            field[319:300]  <= field[299:280];
+            field[339:320]  <= field[319:300];
+            field[359:340]  <= field[359:340];
+            field[379:360]  <= field[379:360];
+            field[399:380]  <= field[399:380];
+
+            total_line_num <= total_line_num + 1; //full line recorder
+        end
+
+        if(line[17]) begin
+            field[19:0]     <= 20'd0;
+            field[39:20]    <= field[19:0]   ;
+            field[59:40]    <= field[39:20]  ;
+            field[79:60]    <= field[59:40]  ;
+            field[99:80]    <= field[79:60]  ;
+            field[119:100]  <= field[99:80]  ;
+            field[139:120]  <= field[119:100];
+            field[159:140]  <= field[139:120];
+            field[179:160]  <= field[159:140];
+            field[199:180]  <= field[179:160];
+            field[219:200]  <= field[199:180];
+            field[239:220]  <= field[219:200];
+            field[259:240]  <= field[239:220];
+            field[279:260]  <= field[259:240];
+            field[299:280]  <= field[279:260];
+            field[319:300]  <= field[299:280];
+            field[339:320]  <= field[319:300];
+            field[359:340]  <= field[339:320];
+            field[379:360]  <= field[379:360];
+            field[399:380]  <= field[399:380];
+
+            total_line_num <= total_line_num + 1; //full line recorder
+        end
+
+        if(line[18]) begin
+            field[19:0]     <= 20'd0;
+            field[39:20]    <= field[19:0]   ;
+            field[59:40]    <= field[39:20]  ;
+            field[79:60]    <= field[59:40]  ;
+            field[99:80]    <= field[79:60]  ;
+            field[119:100]  <= field[99:80]  ;
+            field[139:120]  <= field[119:100];
+            field[159:140]  <= field[139:120];
+            field[179:160]  <= field[159:140];
+            field[199:180]  <= field[179:160];
+            field[219:200]  <= field[199:180];
+            field[239:220]  <= field[219:200];
+            field[259:240]  <= field[239:220];
+            field[279:260]  <= field[259:240];
+            field[299:280]  <= field[279:260];
+            field[319:300]  <= field[299:280];
+            field[339:320]  <= field[319:300];
+            field[359:340]  <= field[339:320];
+            field[379:360]  <= field[359:340];
+            field[399:380]  <= field[399:380];
+
+            total_line_num <= total_line_num + 1; //full line recorder
+        end
+
+        if(line[19]) begin
+            field[19:0]     <= 20'd0;
+            field[39:20]    <= field[19:0]   ;
+            field[59:40]    <= field[39:20]  ;
+            field[79:60]    <= field[59:40]  ;
+            field[99:80]    <= field[79:60]  ;
+            field[119:100]  <= field[99:80]  ;
+            field[139:120]  <= field[119:100];
+            field[159:140]  <= field[139:120];
+            field[179:160]  <= field[159:140];
+            field[199:180]  <= field[179:160];
+            field[219:200]  <= field[199:180];
+            field[239:220]  <= field[219:200];
+            field[259:240]  <= field[239:220];
+            field[279:260]  <= field[259:240];
+            field[299:280]  <= field[279:260];
+            field[319:300]  <= field[299:280];
+            field[339:320]  <= field[319:300];
+            field[359:340]  <= field[339:320];
+            field[379:360]  <= field[359:340];
+            field[399:380]  <= field[379:360];
+
+            total_line_num <= total_line_num + 1; //full line recorder
+        end
     
 end
 
