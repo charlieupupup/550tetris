5d8bac8d47401404b479e0b0b1d929c93d416db6
charlieupupup <41690273+charlieupupup@users.noreply.github.com>
2019-12-09 20:51:55 -0500

    Update block_choice.v

--- tetris_v/block_choice.v
+++ tetris_v/block_choice.v
@@ -7,20 +7,72 @@ reg [9:0] rotate_tmp;
 assign rotate_tmp = rotate % 4;
 
 
-reg [15:0] block_0_0 = 16'b0010001000100010;
-reg [15:0] block_1_0 = 16'b0000011001100000;
-reg [15:0] block_2_0 = 16'b0000110001100000;
-reg [15:0] block_3_0 = 16'b0100110001000000;
-reg [15:0] block_4_0 = 16'b1000100010001100;
+
 reg [15:0] block_matrix;
+
+reg[15:0] block_0_0 = 16'b0010001000100010;
+reg[15:0] block_0_1 =
+reg[15:0] block_0_2 =
+reg[15:0] block_0_3 =
+reg[15:0] block_1_0 = 16'b0000011001100000;
+reg[15:0] block_1_1 =
+reg[15:0] block_1_2 =
+reg[15:0] block_1_3 =
+reg[15:0] block_2_0 = 16'b0000110001100000;
+reg[15:0] block_2_1 =
+reg[15:0] block_2_2 =
+reg[15:0] block_2_3 =
+reg[15:0] block_3_0 = 16'b0100110001000000;
+reg[15:0] block_3_1 =
+reg[15:0] block_3_2 =
+reg[15:0] block_3_3 =
+reg[15:0] block_4_0 = 16'b1000100010001100;
+reg[15:0] block_4_1 =
+reg[15:0] block_4_2 =
+reg[15:0] block_4_3 =
+
+
 
 always @ (*)
 case (block_num)
-    0: block_matrix = block_0;
-    1: block_matrix = block_1;
-    2: block_matrix = block_2;
-    3: block_matrix = block_3;
-    4: block_matrix = block_4;
+    0: case (rotate_tmp)
+        0:    block_matrix = block_0_0
+        1:    block_matrix = block_0_1
+        2:    block_matrix = block_0_2
+        3:    block_matrix = block_0_3
+    endcase
+
+    1: case (rotate_tmp)
+        0:    block_matrix = block_1_0
+        1:    block_matrix = block_1_1
+        2:    block_matrix = block_1_2
+        3:    block_matrix = block_1_3
+    endcase
+
+    2: case (rotate_tmp)
+        0:    block_matrix = block_2_0
+        1:    block_matrix = block_2_1
+        2:    block_matrix = block_2_2
+        3:    block_matrix = block_2_3
+    endcase
+
+    3: case (rotate_tmp)
+        0:    block_matrix = block_3_0
+        1:    block_matrix = block_3_1
+        2:    block_matrix = block_3_2
+        3:    block_matrix = block_3_3
+    endcase
+
+    4: case (rotate_tmp)
+        0:    block_matrix = block_4_0
+        1:    block_matrix = block_4_1
+        2:    block_matrix = block_4_2
+        3:    block_matrix = block_4_3
+    endcase
+
+
+
+
     default: block_matrix = block_0_0;
 endcase
 endmodule // block_choice
