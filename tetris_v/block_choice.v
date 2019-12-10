97f65cd99caf370d49e154cb621b77b3aaf8a14e
charlieupupup <41690273+charlieupupup@users.noreply.github.com>
2019-12-09 21:20:27 -0500

    Update block_choice.v

--- tetris_v/block_choice.v
+++ tetris_v/block_choice.v
@@ -10,26 +10,26 @@ assign rotate_tmp = rotate % 4;
 
 reg [15:0] block_matrix;
 
-reg[15:0] block_0_0 = 16'b0010001000100010;
-reg[15:0] block_0_1 =
-reg[15:0] block_0_2 =
-reg[15:0] block_0_3 =
+reg[15:0] block_0_0 = 16'b0100010001000100;
+reg[15:0] block_0_1 = 16'b0000111100000000;
+reg[15:0] block_0_2 = 16'b0010001000100010;
+reg[15:0] block_0_3 = 16'b0000000011110000;
 reg[15:0] block_1_0 = 16'b0000011001100000;
-reg[15:0] block_1_1 =
-reg[15:0] block_1_2 =
-reg[15:0] block_1_3 =
+reg[15:0] block_1_1 = 16'b0000011001100000;
+reg[15:0] block_1_2 = 16'b0000011001100000;
+reg[15:0] block_1_3 = 16'b0000011001100000;
 reg[15:0] block_2_0 = 16'b0000110001100000;
-reg[15:0] block_2_1 =
-reg[15:0] block_2_2 =
-reg[15:0] block_2_3 =
+reg[15:0] block_2_1 = 16'b0000110001100000;
+reg[15:0] block_2_2 = 16'b0000110001100000;
+reg[15:0] block_2_3 = 16'b0000110001100000;
 reg[15:0] block_3_0 = 16'b0100110001000000;
-reg[15:0] block_3_1 =
-reg[15:0] block_3_2 =
-reg[15:0] block_3_3 =
+reg[15:0] block_3_1 = 16'b0010011100000000;
+reg[15:0] block_3_2 = 16'b0000001000110010;
+reg[15:0] block_3_3 = 16'b0000000011100100;
 reg[15:0] block_4_0 = 16'b1000100010001100;
-reg[15:0] block_4_1 =
-reg[15:0] block_4_2 =
-reg[15:0] block_4_3 =
+reg[15:0] block_4_1 = 16'b1111100000000000;
+reg[15:0] block_4_2 = 16'b0011000100010001;
+reg[15:0] block_4_3 = 16'b0000000000011111;
 
 
 
@@ -69,9 +69,6 @@ case (block_num)
         2:    block_matrix = block_4_2
         3:    block_matrix = block_4_3
     endcase
-
-
-
 
     default: block_matrix = block_0_0;
 endcase
